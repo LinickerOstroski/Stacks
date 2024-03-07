9e7fe1a15b9b2aa7a5ffddb24000f2f6dca7111f
LinickerOstroski <olinicker@gmail.com>
2024-03-07 20:56:50 -0300

    initial commit

---
+++ src/main/java/pilhajava/Pilha.java
@@ -0,0 +1,72 @@
+package pilhajava;
+
+public class Pilha<T> {
+
+    private int topo;
+    private T[] elementos;
+
+        public Pilha(int tamanho) { //Construtor
+        elementos = (T[]) new Object[tamanho];
+        this.topo = -1;
+
+    }
+
+    public boolean isFull() {
+        return this.topo == elementos.length - 1; // teste lógico.
+        /*
+        if(this.topo == elementos.length - 1) {
+            return true;
+        } else {
+            return false;
+        }
+         */
+    }
+
+    public boolean isEmpty() {
+        //return this.topo == -1;
+
+        if (this.topo == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean push(T novoDado) {
+        if (!this.isFull()) { // ! = se não acontecer
+            elementos[++topo] = novoDado;
+            /*primeiro implementando no topo, depois o valor*/
+            return true;
+        } // fim if
+        return false;
+    }// fim push
+
+    public T pop() { //O POP não apaga os dados, ele dá TOPO -1 e o dado fica lá até ser subescrito
+        //return elementos[topo--];
+        T retorno = elementos[topo];
+        topo--;
+        return retorno;
+    }// fim pop
+
+    public int size() {
+        return this.topo + 1; //somamos 1 pois começa no -1, se tenho 0, tenho 1 elemento
+    }
+
+    public T peek() {
+        return elementos[topo];
+    }
+
+    @Override
+
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+
+        for (int i = this.topo; i > 0; i--) 
+            retorno.append(elementos[i] + "\n");
+        
+
+        retorno.append("--------");
+        return retorno.toString();
+    }
+
+}
---
+++ src/main/java/pilhajava/PilhaJava.java
@@ -0,0 +1,60 @@
+package pilhajava;
+
+import java.util.Scanner;
+
+public class PilhaJava {
+
+    private static int mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n--- Menu Pilha ---");
+        System.out.println("1. Inserir elemento");
+        System.out.println("2. Remover elemento");
+        System.out.println("3. Mostrar topo");
+        System.out.println("4. Mostrar pilha");
+        System.out.println("0. Sair");
+        System.out.printf("Digite a opção desejada: %n", "UTF-8");
+        return scanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+        int tamanho, op = 0, element;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tamanho da pilha:");
+        tamanho = scanner.nextInt();
+
+        Pilha <Integer> pilha = new Pilha<>(tamanho); //Essa pilha só recebe inteiros.
+
+        do {
+            op = mostrarMenu();
+            switch (op) {
+                case 1:
+                    System.out.println("Digite um numero inteiro");
+                    element = scanner.nextInt();
+                    if (pilha.push(element)) {
+                        System.out.println("insercao realizada");
+                    } else {
+                        System.out.println("Stack Overflow!");
+                    }
+                    //scanner.close(); //fechando o scanner para não ter erro de leitura.
+                    break;
+                
+                case 2:
+                    pilha.pop();
+                    System.out.println("Elemento removido!");
+                    break;
+                
+                case 3:
+                    System.out.println("Topo = " + pilha.peek());
+                    break;
+                    
+                case 4:
+                    System.out.println("Elementos da pilha\n");
+                    System.out.println(pilha);
+                    break;
+
+            }// fim switch
+            System.out.println("Pressione uma tecla para continuar");
+            scanner.nextLine();
+        } while (op != 0);
+    }
+}
